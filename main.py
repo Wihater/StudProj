dc46174913a96cbd9f2455b3942c318cfc6ba498
Wihater <150428992+Wihater@users.noreply.github.com>
2023-11-10 16:45:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 import random
 from PyQt5 import uic
-from negr import Ui_Form
-
-class Test(QMainWindow, Ui_Form):
+class Test(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.a)
-
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         self.run(qp)
-
     def run(self, qp):
         if self.a:
             qp.setBrush(QColor(255, 255, 0))
@@ -23,17 +19,14 @@ class Test(QMainWindow, Ui_Form):
             w = random.randint(10, 50)
             qp.drawEllipse(x, y, w, w)
             self.a = False
-
-
     def a(self):
         print(1231234)
         self.a = True
         self.update()
-
-
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
     w = Test()
     w.show()
+    sys.exit(app.exec_())
     sys.exit(app.exec_())
